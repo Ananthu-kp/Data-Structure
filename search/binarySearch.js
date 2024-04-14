bad1ccbf55876f1f3cae0b5b710d8757c47c2a2a
Ananthakrishnan KP <ananthukp17@gmail.com>
2024-04-15 02:26:56 +0530

    first commit

--- search/binarySearch.js
+++ search/binarySearch.js
@@ -1,40 +1,40 @@
-let array=[11,23,34,45,56,67,78];
-let target=56;
+let array = [11, 23, 34, 45, 56, 67, 78];
+let target = 56;
 
-function binarySearch(array,target){
-    let leftI=0
-    let rightI=array.length-1
+function binarySearch(array, target) {
+    let leftI = 0
+    let rightI = array.length - 1
 
-    while(leftI<=rightI){
-        const mid=Math.floor((leftI+rightI)/2)
-        if(array[mid]===target){
+    while (leftI <= rightI) {
+        const mid = Math.floor((leftI + rightI) / 2)
+        if (array[mid] === target) {
             return mid
-        }else if(array[mid]<target){
-            leftI=mid+1
-        }else{
-            rightI=mid-1
+        } else if (array[mid] < target) {
+            leftI = mid + 1
+        } else {
+            rightI = mid - 1
         }
     }
     return -1
 }
 
-console.log(binarySearch(array,target))
+console.log(binarySearch(array, target))
 
 //recursion
 
-function binarySearchRecursion(array,target,left,right){
-    if(left > right){
+function binarySearchRecursion(array, target, left, right) {
+    if (left > right) {
         return -1
     }
-    const mid=Math.floor((left+right)/2)
+    const mid = Math.floor((left + right) / 2)
 
-    if(array[mid]==target){
+    if (array[mid] == target) {
         return mid
-    }else if(array[mid]<target){
-        return binarySearchRecursion(array,target,mid+1,right)
-    }else{
-        return binarySearchRecursion(array,target,mid-1,left)
+    } else if (array[mid] < target) {
+        return binarySearchRecursion(array, target, mid + 1, right)
+    } else {
+        return binarySearchRecursion(array, target, left, mid - 1)
     }
 }
 
-console.log(binarySearchRecursion(array,target,0,array.length-1));
+console.log(binarySearchRecursion(array, target, 0, array.length - 1));
