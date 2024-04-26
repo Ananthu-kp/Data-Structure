6c5b7be80265efc7f25c5e6efba732054c85d491
Ananthakrishnan KP <ananthukp17@gmail.com>
2024-04-26 22:39:41 +0530

    first commit

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -1,18 +1,18 @@
-const array=[7,4,-1,3,0,5]
+const array = [7, 4, -1, 3, 0, 5]
 
-function bubbleSort(array){
+function bubbleSort(array) {
     let swapped;
     do {
-        swapped=false
-        for(let i=0;i<array.length-1;i++){
-            if(array[i]>array[i+1]){
-                let temp=array[i]
-                array[i]=array[i+1]
-                array[i+1]=temp
-                swapped=true
+        swapped = false
+        for (let i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                let temp = array[i]
+                array[i] = array[i + 1]
+                array[i + 1] = temp
+                swapped = true
             }
         }
-    } while (swapped);
+    } while (swapped)
     return array
 }
 
