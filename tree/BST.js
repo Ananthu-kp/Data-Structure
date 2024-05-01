7082cc964468b1ae13991b35323af89afc7547cc
Ananthakrishnan KP <ananthukp17@gmail.com>
2024-05-01 12:47:55 +0530

    tree-BST

---
+++ tree/BST.js
@@ -0,0 +1,44 @@
+class TreeNode {
+    constructor(data) {
+        this.data = data
+        this.left = null
+        this.right = null
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null
+    }
+
+    isEmpty() {
+        return this.root === null
+    }
+
+    insert(data) {
+        let newNode = new TreeNode(data)
+        if (!this.root) {
+            this.root = newNode
+        } else {
+            this.insertNode(this.root, newNode)
+        }
+    }
+
+    insertNode(root, newNode) {
+        if (newNode.data < root.data) {
+            if (!root.left) {
+                root.left = newNode
+            } else {
+                this.insertNode(root.left, newNode)
+            }
+        } else {
+            if (!root.right) {
+                root.right = newNode
+            } else {
+                this.insertNode(root.right, newNode)
+            }
+        }
+    }
+}
+
+const bst = new BinarySearchTree()
